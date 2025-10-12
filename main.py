513ef3f28f247417bc0f7629e8b58062c0cb7ecb
Daniel Cabrera A. <daniel.cabreraa@uanl.edu.mx>
2025-10-11 21:09:12 -0600

    Actualizar consultarReservaciones() en main.py

--- main.py
+++ main.py
@@ -200,8 +200,8 @@ def exportarXLSX(fecha:dt.datetime, diccionario:dict):
 
 def consultarReservaciones():
     if not reservaciones:
-        print("ⓘ No hay reservaciones. Registra una ahora:")
-        reservarSala()
+        print("ⓘ No hay reservaciones.")
+        return
     print("Para consultar una reservación, ingresa la fecha (dd/mm/aaaa) bajo la que fue agendada.")
     print("Escribe '0' en cualquier campo para cancelar la operación.")
     while True:
